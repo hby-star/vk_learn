6c40bac8336859d9e7dd32192b4c81b7256f36a8
HouBinyang <2171463703@qq.com>
2025-03-10 14:42:06 +0800

    feat: print physical device.

--- environment_test/EnvTest.cpp
+++ environment_test/EnvTest.cpp
@@ -2,19 +2,58 @@
 #include <GLFW/glfw3.h>
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp> 
+#include <glm/mat4x4.hpp> 
 #include <iostream>
 #include <filesystem>
+#include <vector>
+
+// 打印物理设备名称
+void printPhysicalDeviceNames(VkInstance instance)
+{
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+	if (deviceCount == 0)
+	{
+		std::cout << "No Vulkan physical devices found." << std::endl;
+		return;
+	}
+
+	std::vector<VkPhysicalDevice> physicalDevices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, physicalDevices.data());
+
+	std::cout << "Available Vulkan physical devices:" << std::endl;
+	for (const auto& device : physicalDevices)
+	{
+		VkPhysicalDeviceProperties deviceProperties;
+		vkGetPhysicalDeviceProperties(device, &deviceProperties);
+		std::cout << deviceProperties.deviceName << std::endl;
+	}
+}
 
 int main()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
 	uint32_t extensionCount = 0;
 	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 	std::cout << extensionCount << " extensions supported" << std::endl;
+
+	VkInstance instance;
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+	{
+		std::cerr << "Failed to create Vulkan instance!" << std::endl;
+		return -1;
+	}
+
+	printPhysicalDeviceNames(instance);
+
 	glm::mat4 matrix;
 	glm::vec4 vec;
 	auto test = matrix * vec;
@@ -23,6 +62,9 @@ int main()
 	{
 		glfwPollEvents();
 	}
+
+	vkDestroyInstance(instance, nullptr);
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
